b3b026dbd9d991ecda56d138fbbcf61b97f82595
特别关心 <59378231+Special-Care@users.noreply.github.com>
2024-10-28 08:36:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -146,7 +146,7 @@ export default {
 							sub = 'noTLS.fxxk.dedyn.io'; 
 							subconfig = env.SUBCONFIG || 'https://raw.githubusercontent.com/Special-Care/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini';
 						} else {
-							sub = '0happy.1949.buzz';
+							sub = 'happy.1949.buzz';
 							subconfig = env.SUBCONFIG || "https://raw.githubusercontent.com/Special-Care/TunnelAPI/main/ACL4SSR_Online_Full_MultiMode.ini";
 						}
 					}
